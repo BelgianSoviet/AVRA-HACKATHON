a98957e19b4280a13d609d3653ab9e13c916f9aa
BelgianSoviet <34479138+BelgianSoviet@users.noreply.github.com>
2018-04-20 23:08:25 +0200

    added animation script

---
+++ AVRA - HACKATHON/Assets/Scripts/PlayAnimations.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimations : MonoBehaviour {
+
+    public GameObject startObj;
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        PlayAnim(startObj);
+	}
+    void PlayAnim(GameObject startObj)
+    {
+        startObj.GetComponent<Animation>().Play();
+    }
+}
