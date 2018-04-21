a606a03bdcba244d4574f23d81c2f910db483590
Noirne <richez.antoine.richez@gmail.com>
2018-04-21 08:36:37 +0200

    Cursor added

---
+++ AVRA - HACKATHON/Assets/Scripts/GazeResponder.cs
@@ -0,0 +1,45 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Script
+{
+    class GazeResponder : MonoBehaviour, IFocusable
+    {
+        private Material[] defaultMaterials;
+
+        private void Start()
+        {
+            //defaultMaterials = GetComponent<Renderer>().materials;
+        }
+
+        public void OnFocusEnter()
+        {
+             /*for (int i = 0; i < defaultMaterials.Length; i++)
+             {
+                // Highlight the material when gaze enters using the shader property.
+                defaultMaterials[i].SetFloat("_Gloss", 10.0f);
+             }*/
+        }
+
+        public void OnFocusExit()
+         {
+             /*for (int i = 0; i < defaultMaterials.Length; i++)
+             {
+                // Remove highlight on material when gaze exits.
+                defaultMaterials[i].SetFloat("_Gloss", 1.0f);
+             }*/
+        }
+
+        private void OnDestroy()
+        {
+             foreach (var material in defaultMaterials)
+             {
+                Destroy(material);
+             }
+        }
+    }
+}
---
+++ AVRA - HACKATHON/Assets/Scripts/TapResponder.cs
@@ -0,0 +1,40 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Script
+{
+    public class TapResponder : MonoBehaviour, IInputClickHandler
+    {
+        public Text displayText;
+        public static int Click;
+
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            GameObject gameObject;
+            if((gameObject = eventData.selectedObject.gameObject).tag == "Button")
+            {
+                Debug.Log("OKOK");
+                switch (gameObject.GetComponentInChildren<Text>().text)
+                {
+                    case "Button1":
+                        Click++;
+                        displayText.text = "Click " + Click;
+                        break;
+                    case "Button2":
+                        System.Random random = new System.Random();
+                        String[] test = new string[] { "Test1", "Test2", "Test3" };
+                        gameObject.GetComponentInChildren<Text>().text = test[random.Next(0, 2)];
+                        SceneManager.LoadScene("Test");
+                        break;
+                }
+            }
+            eventData.Use(); // Mark the event as used, so it doesn't fall through to other handlers.
+        }
+    }
+}
