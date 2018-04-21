eaa8646f89004055666b460f83e43e4f03266e7d
BelgianSoviet <34479138+BelgianSoviet@users.noreply.github.com>
2018-04-21 04:04:44 +0200

    I'm an idiot... but it works

---
+++ AVRA - HACKATHON/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    string startScene = "Start";
+    string workingScene = "Working";
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene(startScene);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- AVRA - HACKATHON/Assets/Scripts/PlayAnimations.cs
+++ AVRA - HACKATHON/Assets/Scripts/PlayAnimations.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayAnimations : MonoBehaviour {
 
     public GameObject startObj;
+    Animation anim;
 	// Use this for initialization
 	void Start ()
     {
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        PlayAnim(startObj);
-	}
-    void PlayAnim(GameObject startObj)
-    {
-        startObj.GetComponent<Animation>().Play();
+        anim = startObj.GetComponent<Animation>();
+        StartCoroutine(this.PlayAnim());
     }
+    // Update is called once per frame
+    void Update ()
+    {
+    }
+    IEnumerator PlayAnim()
+    {
+        anim.Play();
+        yield return new WaitForSeconds(anim.clip.length);
+        SceneManager.LoadScene("Working");
+    }
+
 }
